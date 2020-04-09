b6931b8f26555251e79b7d726bea1fa1e25ea3eb
cpeadkins <cpeadkins@gmail.com>
2020-04-09 19:56:44 -0400

    Revisions to puts statement

--- lib/cli.rb
+++ lib/cli.rb
@@ -18,11 +18,11 @@ class CLI
                 user_year = gets.strip
             end
             until user_year.to_i < Date.today.year+1
-                puts "Unfortunately, this API does not predict future information. Tell me ... what is the future like time-traveler? Your birthyear has been set to #{Date.today.year}!"
+                puts "Unfortunately, this API does not predict future information. Tell me ... what is the future like time-traveler? Your birth-year has been set to #{Date.today.year}!"
                 user_year = Date.today.year
             end
             if user_year.to_i < 1995
-                puts "Unfortunately, this API does not contain information before 1995. Congratulations on finding the fountain of youth! Your birthyear is now set to 1996!"
+                puts "Unfortunately, this API does not contain information before 1995. Congratulations on finding the fountain of youth you old geezer! Your birth-year has been set to the default of 1996!"
                 user_year = 1996
             end
         
