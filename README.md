541a97bb02abb66ef7ce1dbc9d9e36b3d2fc067f
cpeadkins <cpeadkins@gmail.com>
2020-04-09 19:38:28 -0400

    Added to error messages to lib/cli.rb and abort line to prevent the program errors from duplicating.

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Project Description
-This purpose of this academic project is to practice Ruby API utilization and user input via Command Line Interface. The application queries the user for a date input (with guidance) and will output an index from NASA's APOD (Astronomy Picture of the Day) API. When presented with an invalid date input, the program should notify the user.
+This purpose of this academic project is to practice Ruby API utilization and user input via Command Line Interface. The application queries the user for a date input (format YYYY-MM-DD with guidance) and will output an index from NASA's APOD (Astronomy Picture of the Day) API. When presented with an invalid date input, the program should notify the user.
 # Installation Guide
+ruby bin/run
 # Contributors Guide
 Being an academic milestone, no contributions outside of the school are expected to be considered. Do not expect a response as this will not be monitored.
 # Licensing Statement
--- lib/cli.rb
+++ lib/cli.rb
@@ -2,7 +2,6 @@ class CLI
     def call_cli
         puts "Welcome to the APOD (Astronomy Picture of the Day) Birthday Finder!"
         input_date
-        # API.new.call_api
     end
     
     def input_date
@@ -11,19 +10,19 @@ class CLI
         puts "What is your birth-year? (Format: 'YYYY')"
         user_year = gets.strip
             until user_year !~ /\D/ #Is there a way to avoid empty space? Invalid.
-                puts "'#{user_year}' is an invalid year input. Please re-enter your birth-year. (Format: 'YYYY')"
+                puts "**ERROR** '#{user_year}' is an invalid year input. Please re-enter your birth-year. (Format: 'YYYY')"
                 user_year = gets.strip
             end
             until user_year.length == 4
-                puts "'#{user_year}' is an invalid year input. Please re-enter your birth-year. (Format: 'YYYY')"
+                puts "**ERROR** '#{user_year}' is an invalid year input. Please re-enter your birth-year. (Format: 'YYYY')"
                 user_year = gets.strip
             end
-            # until user_year.to_i > Date.today.year+1
-            #     puts "Unfortunately, this API does not predict future information. Tell me ... what is the future like time-traveler? Your birthyear has been set to #{Date.today.year}!"
-            #     user_year = Date.today.year
-            # end
+            until user_year.to_i < Date.today.year+1 # Why does this not work? Need to prevent users from inputing future years.
+                puts "**ERROR** Unfortunately, this API does not predict future information. Tell me ... what is the future like time-traveler? Your birthyear has been set to #{Date.today.year}!"
+                user_year = Date.today.year
+            end
             if user_year.to_i < 1995
-                puts "Unfortunately, this API does not contain information before 1995. Congratulations on finding the fountain of youth! Your birthyear is now set to 1996!"
+                puts "**ERROR** Unfortunately, this API does not contain information before 1995. Congratulations on finding the fountain of youth! Your birthyear is now set to 1996!"
                 user_year = 1996
             end
         
@@ -31,15 +30,15 @@ class CLI
         puts "What is your birth-month? (Format: 'MM')"
         user_month = gets.strip
             until user_month !~ /\D/ #Is there a way to avoid empty space? Invalid.
-                puts "'#{user_month}' is an invalid input. Please re-enter your birth-month. (Format: 'MM')"
+                puts "**ERROR** '#{user_month}' is an invalid input. Please re-enter your birth-month. (Format: 'MM')"
                 user_month = gets.strip
             end
             until user_month.length == 2
-                puts "'#{user_month}' is an invalid input. Please re-enter your birth-month. (Format: 'MM')"
+                puts "**ERROR** '#{user_month}' is an invalid input. Please re-enter your birth-month. (Format: 'MM')"
                 user_month = gets.strip
             end
-            until user_month.to_i > 12
-                puts "'#{user_month}' is an invalid input. Please re-enter your birth-month. (Format: 'MM', up to 12)"
+            until user_month.to_i <= 12
+                puts "**ERROR** '#{user_month}' is an invalid input. Please re-enter your birth-month. (Format: 'MM', up to 12)"
                 user_month = gets.strip
             end
         
@@ -47,23 +46,31 @@ class CLI
         puts "What is your birth-day? (Format: 'DD')"
         user_day = gets.strip
             until user_day !~ /\D/ #Is there a way to avoid empty space? Invalid.
-                puts "'#{user_day}' is an invalid input. Please re-enter your birth-month. (Format: 'DD')"
+                puts "**ERROR** '#{user_day}' is an invalid input. Please re-enter your birth-month. (Format: 'DD')"
                 user_day = gets.strip
             end
             until user_day.length == 2
-                puts "'#{user_day}' is an invalid input. Please re-enter your birth-month. (Format: 'DD')"
+                puts "**ERROR** '#{user_day}' is an invalid input. Please re-enter your birth-month. (Format: 'DD')"
                 user_day = gets.strip
             end
-            until user_day.to_i > 31
-                puts "'#{user_day}' is an invalid input. Please re-enter your birth-month. (Format: 'DD', up to 31)"
+            until user_day.to_i <= 31
+                puts "**ERROR** '#{user_day}' is an invalid input. Please re-enter your birth-month. (Format: 'DD', up to 31)"
                 user_day = gets.strip
             end
         
         user_date = "#{user_year}-#{user_month}-#{user_day}"
-        puts ""
-        puts "The birthday you entered is #{user_date}."
-        puts ""
-        API.new.call_api(user_date)
+
+        if Date.valid_date?(user_year.to_i,user_month.to_i,user_day.to_i) == false
+            puts "**ERROR** The date of #{user_date} is invalid or does not exist. Please re-submit."
+            input_date
+            abort
+        else
+            puts ""
+            puts "The birthday you entered is #{user_date}."
+            puts ""
+        end
+
+        API.new.call_api(user_date)   
     end
 
 end
