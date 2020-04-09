84c2cf9d0aa99aaa2fc9dc6d86ccade7e370fa69
cpeadkins <cpeadkins@gmail.com>
2020-04-09 17:12:00 -0400

    Updates and refactors. Working code. Need to add conditional statements for date inputs.

--- lib/api.rb
+++ lib/api.rb
@@ -1,18 +1,17 @@
 class API
 
-    def call_api
-        # user_year =
-        # user_month = gets.strip
-        # user_day = 
-        # user_date = user_year-user_month-user_day
-        user_date = "2000-01-12" #temporary date without user input
-
+    def call_api(user_date)
         resp = RestClient.get("https://api.nasa.gov/planetary/apod?api_key=MbbbqMtQzgF1Hcsg4oz8kTdUH23FsPQgMQsyo9gs&date=#{user_date}")
         query_hash = JSON.parse(resp.body,symbolize_names:true)
         query_date = query_hash[:date]
         query_title = query_hash[:title]
         query_explanation = query_hash[:explanation]
         query_url = query_hash[:url]
+        puts ""
+        puts "Date: #{query_date}"
+        puts "Title: #{query_title}"
+        puts "Description: #{query_explanation}"
+        puts "Image URL: #{query_url}"
     end
 
 end
--- lib/cli.rb
+++ lib/cli.rb
@@ -1,7 +1,20 @@
 class CLI
-
-    def call
-        puts "testing - our program works"
+    def call_cli
+        puts "Welcome to the APOD (Astronomy Picture of the Day) Birthday Finder!"
+        input_date
+        # API.new.call_api
     end
-
-end
+    def input_date
+        puts ""
+        puts "What is your birth-year? (Format: 'YYYY')"
+        user_year = gets.strip
+        puts ""
+        puts "What is your birth-month? (Format: 'MM')"
+        user_month = gets.strip
+        puts ""
+        puts "What is your birth-day? (Format: 'DD')"
+        user_day = gets.strip
+        user_date = "#{user_year}-#{user_month}-#{user_day}"
+        API.new.call_api(user_date)
+    end
+  end
