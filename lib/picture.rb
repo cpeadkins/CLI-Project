7611e7d5e7b998bd8d774265de317e4924596442
cpeadkins <cpeadkins@gmail.com>
2020-04-13 10:09:33 -0400

    Update link pop-up

--- lib/picture.rb
+++ lib/picture.rb
@@ -9,7 +9,7 @@ class Picture
     def display_url
         puts "Image URL: #{@url}"
         puts ""
-        link = "http://stackoverflow.com/"
+        link = "#{@url}"
             if RbConfig::CONFIG['host_os'] =~ /mswin|mingw|cygwin/
             system "start #{link}"
             elsif RbConfig::CONFIG['host_os'] =~ /darwin/
